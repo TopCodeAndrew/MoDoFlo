92c1f67f51cbd8ae7be4ba5ef8a6a0b0521da419
Andrew Smith <topcodeandrew@gmail.com>
2021-01-11 13:38:06 -0700

    basic landing design done

--- src/1-Components/Landing.js
+++ src/1-Components/Landing.js
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from 'react';
+import photo from '../images/Simple-Laptop-banner.jpeg';
 
 export default function Landing() {
 
     return (
         <div className='landing'>
+            <img className='laptop-photo' src={photo} alt='simple laptop' />
 
-            <span>
-                <p> Make Learning</p>
-                <p> SIMPLE </p>
+            <span className='title'>
+                <p id='doing'> Make Doing</p>
+                <p id='simple'> SIMPLE </p>
             </span>
-            <img className='laptop-photo' src='https://images.unsplash.com/photo-1471897488648-5eae4ac6686b?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=3334&q=80' alt='simple laptop' />
         </div>
     )
 }
