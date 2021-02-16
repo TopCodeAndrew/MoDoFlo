ce669e64a28440befa1fafa39d62fed947e23f8a
Andrew Smith <topcodeandrew@gmail.com>
2021-02-16 13:03:46 -0700

    header dropdown styling

--- src/1-Components/Header.js
+++ src/1-Components/Header.js
@@ -11,6 +11,7 @@ function Header(props) {
     const [menuToggle, setMenuToggle] = useState(false)
 
     function logoutUser() {
+        setMenuToggle(false);
         axios
             .delete('/auth/logout')
             .then(async function wait() {
@@ -26,7 +27,7 @@ function Header(props) {
 
     function notLoggedIn() {
         return (
-            <div className='link-container'>
+            <div className='to-auth link-container logged-out'>
                 <Link onClick={() => setMenuToggle(false)} className='link go' to='/login'>
                     LOGIN
                 </Link>
@@ -39,8 +40,8 @@ function Header(props) {
 
     function LoggedIn() {
         return (
-            <div className='link-container'>
-                <Link onClick={() => setMenuToggle(false)} className='link go' onClick={() => logoutUser()} to='/landing'>
+            <div className='to-auth link-container logged-in'>
+                <Link className='link go' onClick={() => logoutUser()} to='/landing'>
                     LOGOUT
                 </Link>
             </div>
@@ -49,26 +50,28 @@ function Header(props) {
 
     return (
         <div className="header">
-            <Link to='/' className='title'> MODOFLO</Link>
+            <div className='top-header'>
+                <Link to='/' className='title'> MODOFLO</Link>
+                <img src={hamburger} className='hamburger' onClick={toggleOpen} />
+            </div>
 
 
             <div className='desktop-menu'>
-                <span className='to-auth'>{props.isLoggedIn ? LoggedIn() : notLoggedIn()}
+                <span className='desktop-menu to-auth'>{props.isLoggedIn ? LoggedIn() : notLoggedIn()}
                 </span>
             </div>
 
 
 
-            <div className='mobile'>
-                <img src={hamburger} className='hamburger' onClick={toggleOpen} />
-                <div className='mobile-menu'> {menuToggle ?
-                    <span className='to-auth'>{!props.isLoggedIn ? notLoggedIn() : LoggedIn()}
-                    </span>
-                    :
-                    null
-                }
-                </div>
+            {/* <div className='mobile'> */}
+            <div className='mobile-menu'> {menuToggle ?
+                <span className='to-auth'>{!props.isLoggedIn ? notLoggedIn() : LoggedIn()}
+                </span>
+                :
+                null
+            }
             </div>
+            {/* </div> */}
 
 
         </div>
